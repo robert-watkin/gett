9c96e8cb48bba60f5d81e05f9c8c5be210c1efd5
Robert Watkin <robert.watkin12@gmail.com>
2026-05-31 17:11:21 +0100

    feat: can make basic get requests

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	flag.Parse()
+	url := flag.Arg(0)
+	if url == "" {
+		log.Fatalf("URL not provided")
+	}
+
+	fmt.Fprintf(os.Stderr, "The provided URL is: %s\n", url)
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Failed to GET: %v", err)
+	}
+	defer response.Body.Close()
+
+	fmt.Println("Response status: ", response.Status)
+
+	io.Copy(os.Stdout, response.Body)
 }
